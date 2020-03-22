d4b3ff376369bd11bc15bf5bf75b708c0dfde780
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 23:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const events=["Birthday"];
 
 function writeCards(names,events) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you,${names[i]}, and added a bow!`);
+    console.log(`Thank you,${names[i]}, for the wonderful ${events[i]} `);
   }
  
   return gifts;
