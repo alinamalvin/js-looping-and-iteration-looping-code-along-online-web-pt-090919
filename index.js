60bbf8049472d01fc7d3be82705a4b6e9d161d3f
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 23:29:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards( names, gift ) {
+  let cards = []
+  for ( let i = 0; i < names.length; i++ ) {
+cards.push( `Thank you, ${names[i]}, for the wonderful ${gift} gift!` )
+ }
+ return cards
+}	
+ 
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
