1ba663e107ebd614e9f1465149935bac525b24e6
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 23:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ cards.push( `Thank you, ${names[i]}, for the wonderful ${gift} gift!` )
  }
  return cards
 }	
-
-const gifts = ["teddy bear", "drone", "doll"];
  
-function wrapGifts(gifts) {
-  let i = 0; // the initialization moved OUTSIDE the body of the loop!
+function countDown(integer) {
+  let i = 1; // the initialization moved OUTSIDE the body of the loop!
   while (i < gifts.length) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
     i++; // the iteration moves INSIDE the body of the loop!
