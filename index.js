d53694634fe9c52d0f4de78e5d8a068934fad6b5
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 22:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+ 
+wrapGifts(gifts);
