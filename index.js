2d454c2c8ae9ce5262389fbdc27f8bcd6c07f564
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 23:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function writeCards( names, gift ) {
   let cards = []
   for ( let i = 0; i < names.length; i++ ) {
 cards.push( `Thank you, ${names[i]}, for the wonderful ${gift} gift!` )
+ }
+  return thankYouCards	  return cards
+}	
