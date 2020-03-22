37aa03e8c2c7316b8a5f2c5d6c25ddb444011ff3
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 23:29:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ cards.push( `Thank you, ${names[i]}, for the wonderful ${gift} gift!` )
 }	
  
 function countDown() {
-  let i = 10;
-  while (i > 0) {
-    console.log(i--);
-  }
-} 
+    let i = 10
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
