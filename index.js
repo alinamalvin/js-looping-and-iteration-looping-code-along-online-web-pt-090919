5fa14142bf89c0a7b6b791d7af4ec6cbb5060193
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-22 23:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function writeCards(names,events) {
-  for (let i = 0; i < names.length; i++) {
+function writeCards(namesArray,events) {
+  for (let i = 0; i < namesArray.length; i++) {
     console.log(`Thank you,${names[i]}, for the wonderful ${events[i]} gift!`);
   }
   return names;
 }
  
-wrapGifts(gifts);
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
